c007ea2de5846e1c403bbf9c451bb22c0fea3c1f
Matthew Meadows <matthewmeadows1999@gmail.com>
2023-10-09 16:23:56 -0400

    Setting up and styling UI for homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,23 @@
-import Image from 'next/image'
+
 
 export default function Home() {
   return (
-   <div>test</div>
+   <main className="p-4">
+    <header className="flex justify-between mb-4">
+      <a href= "">EpicCaptions</a>
+      <nav className="flex gap-6">
+      <a href="">Home</a>
+        <a href="">Pricing</a>
+        <a href="">Contact</a>
+      </nav>
+    </header>
+    <div>
+      <h1>Add epic captions to your videos </h1>
+      <h2>Just upload your files and we will do the rest</h2>
+    </div>
+    <div>
+      <button>Choose file </button>
+    </div>
+   </main>
   )
 }
