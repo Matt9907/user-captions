c7ee8f8878cbcf5e5f6eb393477385f96510d452
Matthew Meadows <matthewmeadows1999@gmail.com>
2023-10-16 15:59:53 -0400

    Setting up function for Layout,js to just call to style headers in UI

---
+++ src/components/PageHeaders.js
@@ -0,0 +1,16 @@
+export default function PageHeaders({
+    h1Text = 'Hello',
+    h2Text = 'Subheader',
+}){
+    return(
+        <section className="text-center mt-12 sm:mt-24 mb-4 sm:mb-8">
+            <h1 className="text-xl sm:text-3xl" style={{textShadow: '1px 1px 0 rgba(0,0,0,.2)'}}>
+            {h1Text}
+            </h1>
+            
+       
+
+        </section>
+
+    )
+}
