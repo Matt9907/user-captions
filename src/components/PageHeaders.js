3c929f40094104294822484caddc84b12e5042fc
Matthew Meadows <matthewmeadows1999@gmail.com>
2023-10-16 16:01:17 -0400

    Finishing headers function

--- src/components/PageHeaders.js
+++ src/components/PageHeaders.js
@@ -7,10 +7,9 @@ export default function PageHeaders({
             <h1 className="text-xl sm:text-3xl" style={{textShadow: '1px 1px 0 rgba(0,0,0,.2)'}}>
             {h1Text}
             </h1>
-            
-       
-
-        </section>
-
-    )
+            <h2 className="text-white/75 text-sm sm:text-base">
+            {h2Text}
+            </h2>
+            </section>
+    );
 }
