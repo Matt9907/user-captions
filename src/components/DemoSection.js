499fb502558734af16a450080bb4e9fc55430325
Matthew Meadows <matthewmeadows1999@gmail.com>
2023-10-17 14:54:18 -0400

    Setting the styling for demo videos

--- src/components/DemoSection.js
+++ src/components/DemoSection.js
@@ -2,7 +2,16 @@
 export default function DemoSection(){
     return(
         <section className="flex justify-around mt-8 sm: mt-12 items-center">
-            
+            <div className="hidden sm:block bg-gray-800/50 w-[240px] rounded-xl overflow-hidden">
+                <video></video>
+            </div>
+            <div className="hidden sm:block">
+
+            </div>
+            <div className="bg-gray-800/50 w-[240px] rounded-xl overflow-hidden">
+                <video></video>
+            </div>
+
         </section>
-    )
+    );
 }
