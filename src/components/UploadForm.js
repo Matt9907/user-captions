f726ee1be8a3d843cfcccaae6328e945bc48a246
Matthew Meadows <matthewmeadows1999@gmail.com>
2023-10-25 15:09:49 -0400

    Setting up Upload Form

---
+++ src/components/UploadForm.js
@@ -0,0 +1,29 @@
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+
+
+
+
+export default function UploadFunction(){
+    const [isUploading, setIsUploading] = useState(false);
+    const router = useRouter();
+
+    
+    
+    
+    
+    
+    
+    
+    return(
+        <>
+        
+        
+        
+        
+        
+        </>
+
+    );
+}
