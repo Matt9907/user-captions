abde5f96db2807e66a7a95c54fdf8ef117740200
Matthew Meadows <matthewmeadows1999@gmail.com>
2023-10-25 15:12:42 -0400

    Logic behind the upload form

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -9,6 +9,21 @@ export default function UploadFunction(){
     const [isUploading, setIsUploading] = useState(false);
     const router = useRouter();
 
+    async function upload (ev){
+        ev.preventDefault();
+        const files = ev.target.files;
+        if(files.length > 0){
+            const file = files[0];
+            setIsUploading(true);
+            const res = await axios.postForm('/api/uplaod',{
+                file,
+            });
+            setIsUploading(false);
+            const newName = res.data.newName;
+            router.push('/'+newName);
+        }
+    }
+
     
     
     
