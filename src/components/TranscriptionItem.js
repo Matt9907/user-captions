e93a6719e5dc3ef8e855b36be63924f9ea7dc601
Matthew Meadows <matthewmeadows1999@gmail.com>
2023-10-31 15:45:48 -0400

    Setting up caption transcription

---
+++ src/components/TranscriptionItem.js
@@ -0,0 +1,19 @@
+export default function TranscriptionItem({
+    item,
+    handleStartTimeChange,
+    handleEndTimeChange,
+    handleContentChange,
+}) {
+    if(!item){
+        return '';
+    }
+    return(
+        <div>
+            
+        </div>
+
+
+
+    );
+
+}
