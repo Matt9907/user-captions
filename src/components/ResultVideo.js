2cace609269ef5ab13c04498ec451575f49c7d3a
Matthew Meadows <matthewmeadows1999@gmail.com>
2023-11-09 16:10:13 -0500

    Setting up states for Results

--- src/components/ResultVideo.js
+++ src/components/ResultVideo.js
@@ -1,7 +1,15 @@
+import { useState } from "react";
 
 
 
 
 export default function ResultVideo({filename, transcriptionItems}){
+    const videoUrl = "" + filename;
+    const [loaded, setLoaded] = useState(false);
+    const [primaryColor, setPrimaryColor] = useState('#FFFFFF');
+    const [outlineColor, setOutlineColor] = useState('#000000');
+    const [progress, setProgress] = useState(1);
+    const ffmpegRef = useRef(new FFmpeg());
+    const videoRef = useRef(null);
 
 }
